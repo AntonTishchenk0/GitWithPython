a078e858051074566cf25917a3be6b57cd327e47
Anton <AntonTishchenk0@yandex.ru>
2023-11-23 01:51:14 +0200

    ?

--- GitWithPy.py
+++ GitWithPy.py
@@ -65,11 +65,3 @@
 
 # Дано натуральное число n. Напишите программу, 
 # которая печатает численный треугольник с высотой равной n.
-
-n = int(input())
-total = 0
-for i in range(1, n + 1):
-    for j in range(i):
-        total += 1
-        print(total, end=' ')
-    print()
