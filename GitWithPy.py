01befee282ba788cd3b7958271c6ce102d699bc1
Anton <AntonTishchenk0@yandex.ru>
2023-11-22 03:24:48 +0200

    added Task2

--- GitWithPy.py
+++ GitWithPy.py
@@ -12,3 +12,19 @@
 #             if 10 * b + 5 * k + 0.5 * t == 100:
 #                 if b + k + t == 100:
 #                     print('b =', b, 'k =', k, 't =', t)
+
+# Task 2:
+
+# На вход программе подается два натуральных числа a и b (a < b). 
+# Напишите программу, которая находит все простые числа от a до b включительно.
+
+# a = int(input('Input a first number: '))
+# b = int(input('Input a second number: '))
+# for i in range(a, b + 1):
+#     if i == 1:
+#         continue 
+#     for j in range(2, i):
+#         if i % j == 0:
+#             break 
+#     else:
+#         print(i)
